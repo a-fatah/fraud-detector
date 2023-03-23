f45506572448bbab64f9797a3a941a7293f77348
a-fatah <abdulfatah66@gmail.com>
2023-03-23 12:30:41 +0100

    configure streams

--- src/main/java/io/freevariable/trustnet/frauddetector/stream/StreamsConfig.java
+++ src/main/java/io/freevariable/trustnet/frauddetector/stream/StreamsConfig.java
@@ -1,14 +1,36 @@
 package io.freevariable.trustnet.frauddetector.stream;
 
+import io.freevariable.trustnet.frauddetector.model.Transaction;
+import lombok.val;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
 import org.springframework.kafka.config.KafkaStreamsConfiguration;
 import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 
+import java.util.HashMap;
+
+import static org.apache.kafka.streams.StreamsConfig.*;
+
 @Configuration
 public class StreamsConfig {
+
+    @Value("${spring.kafka.bootstrap-servers})")
+    private String bootstrapAddress;
+
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    KafkaStreamsConfiguration kStreamsConfig() {
+        val props = new HashMap<String, Object>();
+        props.put(APPLICATION_ID_CONFIG, "fraud-detector");
+        props.put(BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.serdeFrom(Transaction.class).getClass().getName());
+        return new KafkaStreamsConfiguration(props);
+    }
 
     @Bean
     public StreamsBuilderFactoryBean kafkaStreamsBuilder(KafkaStreamsConfiguration streamsConfig) {
@@ -20,8 +42,4 @@ public class StreamsConfig {
         return new KafkaStreams(streamsBuilder.build(), streamsConfig.asProperties());
     }
 
-    @Bean
-    KafkaStreamsRunner kafkaStreamsRunner(KafkaStreams streams) {
-        return new KafkaStreamsRunner(streams);
-    }
 }
