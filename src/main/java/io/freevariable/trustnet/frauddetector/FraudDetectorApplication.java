e0cf787fd3542b0ea3c9e4b8bf6fccef84bfddc2
a-fatah <abdulfatah66@gmail.com>
2023-03-23 11:32:25 +0100

    add fraud detector module

---
+++ src/main/java/io/freevariable/trustnet/frauddetector/FraudDetectorApplication.java
@@ -0,0 +1,13 @@
+package io.freevariable.trustnet.frauddetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudDetectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/freevariable/trustnet/frauddetector/FraudDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.freevariable.trustnet.frauddetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
