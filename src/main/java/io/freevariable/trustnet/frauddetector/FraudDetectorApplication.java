aa37af508f02375dcb0bb8c4d6ede1ad358a9df1
a-fatah <abdulfatah66@gmail.com>
2023-03-26 04:03:24 +0200

    use spring cloud stream

--- src/main/java/io/freevariable/trustnet/frauddetector/FraudDetectorApplication.java
+++ src/main/java/io/freevariable/trustnet/frauddetector/FraudDetectorApplication.java
@@ -1,10 +1,16 @@
 package io.freevariable.trustnet.frauddetector;
 
-import io.freevariable.trustnet.frauddetector.stream.KafkaStreamsRunner;
-import org.springframework.boot.CommandLineRunner;
+import io.freevariable.trustnet.frauddetector.model.Transaction;
+import io.freevariable.trustnet.frauddetector.service.FraudDetectorService;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
+import java.util.function.Function;
 
 @SpringBootApplication
 public class FraudDetectorApplication {
@@ -14,10 +20,16 @@ public class FraudDetectorApplication {
     }
 
     @Bean
-    public CommandLineRunner clr(KafkaStreamsRunner runner) {
-        return args -> {
-            System.out.println("Running stream...");
-            runner.start();
-        };
+    Function<KStream<String, Transaction>, KStream<String, Long>> fraudDetector(FraudDetectorService fraudDetectorService) {
+        return stream ->
+                stream.filter((key, transaction) ->
+                        fraudDetectorService.isFraudulent(transaction))
+                        .map((key, transaction) -> KeyValue.pair(transaction.getAccountId(), transaction))
+                        .groupByKey()
+                        .windowedBy(TimeWindows.ofSizeAndGrace(Duration.ofMinutes(5), Duration.ofMinutes(1)))
+                        .count()
+                        .filter((accountId, count) -> count >= 5)
+                        .toStream()
+                        .map((windowedAccountId, count) -> KeyValue.pair(windowedAccountId.key(), count));
     }
 }
