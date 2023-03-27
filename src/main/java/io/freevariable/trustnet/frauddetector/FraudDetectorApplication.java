c61827e85e7036f537389f59f982a173cf1fffae
a-fatah <abdulfatah66@gmail.com>
2023-03-27 09:59:34 +0200

    use spring cloud stream

--- src/main/java/io/freevariable/trustnet/frauddetector/FraudDetectorApplication.java
+++ src/main/java/io/freevariable/trustnet/frauddetector/FraudDetectorApplication.java
@@ -1,15 +1,19 @@
 package io.freevariable.trustnet.frauddetector;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.freevariable.trustnet.frauddetector.model.Transaction;
 import io.freevariable.trustnet.frauddetector.service.FraudDetectorService;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.function.Function;
 
 @SpringBootApplication
@@ -26,10 +30,18 @@ public class FraudDetectorApplication {
                         fraudDetectorService.isFraudulent(transaction))
                         .map((key, transaction) -> KeyValue.pair(transaction.getAccountId(), transaction))
                         .groupByKey()
-                        .windowedBy(TimeWindows.ofSizeAndGrace(Duration.ofMinutes(5), Duration.ofMinutes(1)))
+                        .windowedBy(TimeWindows.ofSizeAndGrace(Duration.ofSeconds(5), Duration.ofSeconds(1)))
                         .count()
                         .filter((accountId, count) -> count >= 5)
                         .toStream()
                         .map((windowedAccountId, count) -> KeyValue.pair(windowedAccountId.key(), count));
     }
+
+     @Bean
+     public ObjectMapper objectMapper() {
+         ObjectMapper objectMapper = new ObjectMapper();
+         objectMapper.registerModule(new JavaTimeModule());
+         return objectMapper;
+     }
+
 }
--- src/main/java/io/freevariable/trustnet/frauddetector/model/Transaction.java
+++ src/main/java/io/freevariable/trustnet/frauddetector/model/Transaction.java
@@ -2,10 +2,12 @@ package io.freevariable.trustnet.frauddetector.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Transaction {
     String transactionId;
